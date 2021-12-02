93b62b28890ea4c048569f4ebf638481c9aa86be
wangsheng <14213145@qq.com>
2021-12-02 14:22:03 +0800

    高亮显示

--- src/test/java/com/windsun/wangs/HotelIndexTest.java
+++ src/test/java/com/windsun/wangs/HotelIndexTest.java
@@ -18,15 +18,19 @@ import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
 import org.elasticsearch.search.sort.SortOrder;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.CollectionUtils;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 import static com.windsun.wangs.constants.HotelIndexConstants.MAPPING_TEMPLATE;
 
@@ -85,15 +89,34 @@ public class HotelIndexTest {
         handleResponse(response);
     }
 
+    // 高亮查询
+    @Test
+    void testHighlight() throws IOException {
+        // 1.准备request
+        SearchRequest request = new SearchRequest("hotel");
+        // 2.准备dsl
+        request.source().query(QueryBuilders.matchQuery("all", "汉庭"));
+        // mame高亮，不精确匹配，只要查询出来就高亮
+        // 设置为 true 以仅在查询匹配该字段时才突出显示该字段。默认为 false 意味着术语在所有请求的字段上都突出显示，
+        // 无论查询是否特别匹配它们。
+        request.source().highlighter(new HighlightBuilder().field("name").requireFieldMatch(false));
+        // 3.发送请求
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        // 4.解析结果
+        handleResponse(response);
+    }
+
     // 分页查询
     @Test
     void testPageMatchAll() throws IOException {
+        // 页码，每页大小
+        int page = 2, size = 5;
         // 1.准备request
         SearchRequest request = new SearchRequest("hotel");
         // 2.准备dsl
         request.source().query(QueryBuilders.matchAllQuery());
         // 分页，也可以链式编程
-        request.source().from(0).size(25).sort("price", SortOrder.ASC);
+        request.source().from((page - 1) * size).size(5).sort("price", SortOrder.ASC);
         // 价格升序
         //request.source().sort("price", SortOrder.ASC);
         // 3.发送请求
@@ -108,7 +131,7 @@ public class HotelIndexTest {
         // 1.准备request
         SearchRequest request = new SearchRequest("hotel");
         // 2.准备dsl
-        request.source().query(QueryBuilders.matchQuery("all","如家"));
+        request.source().query(QueryBuilders.matchQuery("all", "如家"));
         // 3.发送请求
         SearchResponse response = client.search(request, RequestOptions.DEFAULT);
         handleResponse(response);
@@ -120,7 +143,7 @@ public class HotelIndexTest {
         // 1.准备request
         SearchRequest request = new SearchRequest("hotel");
         // 2.准备dsl
-        request.source().query(QueryBuilders.termQuery("city","上海"));
+        request.source().query(QueryBuilders.termQuery("city", "上海"));
         // 3.发送请求
         SearchResponse response = client.search(request, RequestOptions.DEFAULT);
         // 4.解析结果
@@ -148,7 +171,7 @@ public class HotelIndexTest {
         // 创建布尔查询
         BoolQueryBuilder boolQuery = QueryBuilders.boolQuery();
         // 添加must条件
-        boolQuery.must(QueryBuilders.termQuery("city","上海"));
+        boolQuery.must(QueryBuilders.termQuery("city", "上海"));
         // 添加filter条件：小于等于250
         boolQuery.filter(QueryBuilders.rangeQuery("price").lte(150));
         // 2.准备dsl
@@ -165,11 +188,25 @@ public class HotelIndexTest {
         // 4.1 获取总条数
         long total = searchHits.getTotalHits().value;
         System.out.println("总条数：" + total);
-        // 4.2 获取响应结果
+        // 4.2 文档数组
         SearchHit[] hits = searchHits.getHits();
+        // 遍历
         for (SearchHit hit : hits) {
+            // 获取文档source
             String sourceAsString = hit.getSourceAsString();
+            // 反序列化
             HotelDoc hotelDoc = JSON.parseObject(sourceAsString, HotelDoc.class);
+            // 获取高亮结果
+            Map<String, HighlightField> highlightFields = hit.getHighlightFields();
+            if (!CollectionUtils.isEmpty(highlightFields)) {
+                // 根据字段名称获取高亮结果
+                HighlightField highlightField = highlightFields.get("name");
+                if (highlightField != null) {
+                    // 获取高亮值
+                    String name = highlightField.getFragments()[0].toString();
+                    hotelDoc.setName(name);
+                }
+            }
             System.out.println(hotelDoc.toString());
         }
     }
