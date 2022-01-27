c0ce45a490e1c412d8819acb1d56a3be8c35e582
wangsheng <14213145@qq.com>
2022-01-27 21:28:40 +0800

    arraylist 线程不安全的演示

---
+++ src/main/java/com/windsun/wangs/thread/ArrayListThread.java
@@ -0,0 +1,27 @@
+package com.windsun.wangs.thread;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @ClassName : ArrayListThread
+ * @Description : 演示arraylist 线程不安全
+ * @Author : ws
+ * @Date: 2022-01-19 10:47
+ * @Version 1.0
+ */
+public class ArrayListThread {
+
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < 30; i++) {
+            new Thread(() -> {
+                list.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(list);
+            }, String.valueOf(i)).start();
+        }
+    }
+}
